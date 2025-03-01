c9d194d7f09ea9e1ed64125c12a6282da16cba21
Jonathan D. Flores Franco <hiruma5436@gmail.com>
2025-03-01 13:21:13 -0600

    Se agrego hook para que tome una captura si falla el test

--- wdio.conf.js
+++ wdio.conf.js
@@ -261,11 +261,11 @@ export const config = {
      * @param {boolean} result.passed    true if test has passed, otherwise false
      * @param {object}  result.retries   information about spec related retries, e.g. `{ attempts: 0, limit: 0 }`
      */
-    // afterTest: async function(test, context, { error }) {
-    //     if (error) {
-    //         await browser.takeScreenshot();
-    //     }
-    // },
+    afterTest: async function(test, context, { error }) {
+        if (error) {
+            await browser.takeScreenshot();
+        }
+    },
 
 
     /**
